189da909e7c8a29625d2f01d3843bd507f73e540
Michael Zhao <michael.zhao@arm.com>
2019-12-31 08:34:18 +0000

    Build command line arguments.
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    println!("config: {:?}", matches.value_of("config"));
 }
