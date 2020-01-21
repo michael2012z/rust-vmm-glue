f2202a88b417d1a6181a9c77ae9c1a5a488716b4
Michael Zhao <michael.zhao@arm.com>
2020-01-21 02:49:46 -0500

    Integraged vm-memory crate. Initialized guest memory.
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,24 @@
 use crate::error::*;
-use vm_memory::{GuestMemoryMmap, GuestRegionMmap};
+use std::sync::Arc;
+use vm_memory::{GuestAddress, GuestMemoryMmap, GuestRegionMmap, MmapRegion};
+
+#[derive(PartialEq)]
+pub enum RegionType {
+    /// RAM type
+    Ram,
+
+    /// SubRegion memory region.
+    /// A SubRegion is a memory region sub-region, allowing for a region
+    /// to be split into sub regions managed separately.
+    /// For example, the x86 32-bit memory hole is a SubRegion.
+    SubRegion,
+
+    /// Reserved type.
+    /// A Reserved memory region is one that should not be used for memory
+    /// allocation. This type can be used to prevent the VMM from allocating
+    /// memory ranges in a specific address range.
+    Reserved,
+}
 
 pub struct VmMemory {
     guest_mem: Option<GuestMemoryMmap>,
@@ -17,22 +36,39 @@ impl VmMemory {
         Ok(VmMemory { guest_mem: None })
     }
 
-    pub fn memory_regions(&self, size: usize) -> Vec<GuestRegionMmap> {
-        vec![]
+    pub fn arch_memory_regions(&self, size: usize) -> Vec<(GuestAddress, usize, RegionType)> {
+        vec![(GuestAddress(0), size, RegionType::Ram)]
     }
 
-    pub fn memory_regions_init(&self, guest_mem: &GuestMemoryMmap) -> Result<()> {
-        Ok(())
+    fn create_ram_region(
+        &self,
+        start_addr: GuestAddress,
+        size: usize,
+    ) -> Result<Arc<GuestRegionMmap>> {
+        Ok(Arc::new(
+            GuestRegionMmap::new(MmapRegion::new(size).unwrap(), start_addr).unwrap(),
+        ))
     }
 
     pub fn vm_memory_init(&mut self, mem_size_mib: usize) -> Result<()> {
         let mem_size_bytes = mem_size_mib << 20;
-        let mem_regions = self.memory_regions(mem_size_bytes);
-        self.guest_mem = Some(GuestMemoryMmap::from_regions(mem_regions).unwrap());
-        if let Some(guest_mem) = &self.guest_mem {
-            self.memory_regions_init(guest_mem)
-        } else {
-            Err(Error::Generic)
+
+        let arch_mem_regions = self.arch_memory_regions(mem_size_bytes);
+
+        let ram_regions: Vec<(GuestAddress, usize)> = arch_mem_regions
+            .iter()
+            .filter(|r| r.2 == RegionType::Ram)
+            .map(|r| (r.0, r.1))
+            .collect();
+
+        let mut mem_regions = Vec::new();
+        for region in ram_regions.iter() {
+            mem_regions.push(self.create_ram_region(region.0, region.1)?);
         }
+
+        let guest_memory = GuestMemoryMmap::from_arc_regions(mem_regions.clone()).unwrap();
+        self.guest_mem = Some(guest_memory);
+
+        Ok(())
     }
 }
